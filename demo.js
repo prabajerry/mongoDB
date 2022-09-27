368261d4da2f270a12dc3d1c728229ff184d9bc1
prabajerry <padmakalai2017@gmail.com>
2022-09-27 22:02:04 +0530

    add new data in db

--- demo.js
+++ demo.js
@@ -13,32 +13,38 @@ try {
             Bookname : "wings of fire",
             AutherName: "APJ",
             Publishing : "university Press",
-            year:1999
+            year:1999,
+            edition:"paperback edition"
         },
         {
             
             Bookname : "secret",
             AutherName: "Rhonda Byrne",
             Publishing : "Beyond words",
-            year:2006
+            year:2006,
+            edition:"first edition"
         },
         {
             Bookname : "The life",
             AutherName: "Malcolm Knox",
             Publishing : "self Publishing",
-            year:2012
+            year:2012,
+            edition:"Asphalte"
         },
         {
-            Bookname : "The law ofsuccess",
+            Bookname : "The law of success",
             AutherName: "Napoleon Hill",
             Publishing : "Tribeca Books",
-            year:1928
+            year:1928,
+            edition:"The 12st century"
+
         },
         {
             Bookname : "You Can",
             AutherName: " Frank Abagnale jr",
             Publishing : "Grosset & Dunlap",
-            year:1980
+            year:1980,
+            edition:"hardcover edition"
         },
         
         
