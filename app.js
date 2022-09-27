b212a576371659dbd070001e620b994a827be8ed
prabajerry <padmakalai2017@gmail.com>
2022-09-27 22:23:48 +0530

    feature:sanitize,control

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./controler/index')
+const sanitize = require('./sanitization/index')
+const service = require('./service/index')
+
 const app = express();
-const bodyParser=require('body-parser');
+app.use(bodyParser.json());
+
+app.listen(3000, ()=>
+{
+    console.log("server is running");
+})
